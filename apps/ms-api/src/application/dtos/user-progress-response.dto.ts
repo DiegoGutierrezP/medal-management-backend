a67e037fd6bfa1c6120d438c013e22c1886e1324
Diego Gutierrez Pineda <diego.gutierrez@apuestatotal.com>
2025-09-07 12:03:42 -0500

    feat: upd docker compose and readme

--- apps/ms-api/src/application/dtos/user-progress-response.dto.ts
+++ apps/ms-api/src/application/dtos/user-progress-response.dto.ts
@@ -1,7 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserProgressResponseDto {
+  @ApiProperty({
+    description: 'User id',
+  })
   id: number;
+
+  @ApiProperty({
+    description: 'User alias',
+  })
   alias: string;
+
+  @ApiProperty({
+    description: 'total points',
+  })
   totalPoints: number;
+
+  @ApiProperty({
+    description: 'Current medal',
+  })
   medal: string;
+
+  @ApiProperty({
+    description: 'Last updated',
+  })
   lastUpdated?: Date;
 }
--- apps/ms-api/src/application/features/commands/create-user/create-user.command.handler.ts
+++ apps/ms-api/src/application/features/commands/create-user/create-user.command.handler.ts
@@ -1,5 +1,5 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { Logger } from '@nestjs/common';
+import { BadRequestException, Logger } from '@nestjs/common';
 import { CreateUserCommand } from './create-user.command';
 import { UserRepository } from 'apps/ms-api/src/persistence/repositories/user.repository';
 import { UserMapper } from '../../../mappers';
@@ -13,8 +13,17 @@ export class CreateUserCommandHandler
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute({ request }: CreateUserCommand) {
-    const user = await this.userRepository.create(request.alias);
+    try {
+      const user = await this.userRepository.create(request.alias);
 
-    return UserMapper.toUserResponse(user);
+      return UserMapper.toUserResponse(user);
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new BadRequestException('Alias ya existe');
+      }
+      throw new BadRequestException(
+        error.message || 'Ocurrio un error al registrar el usuario',
+      );
+    }
   }
 }
